9ff2e9d21a733561ac585ebc7d93c514d1d2c2a9
ILKHOM <gaffarovilkhom@inbox.ru>
2025-04-27 02:45:46 +0300

    fifth try

--- include/Automata.h
+++ include/Automata.h
@@ -10,29 +10,29 @@ enum STATES {
     WAIT,
     ACCEPT,
     CHECK,
-    COOK,
+    COOK
 };
 
 class Automata {
-    private:
-        int cash;
-        std::vector<std::string> menu;
-        std::vector<int> prices;
-        STATES state;
-        int currentDrink;
+ private:
+    int cash;
+    std::vector<std::string> menu;
+    std::vector<int> prices;
+    STATES state;
+    int currentDrink;
 
-    public:
-        Automata();
-        STATES getState();
-        void getMenu();
-        void on();
-        void off();
-        void coin(int);
-        void cancel();
-        void choice(int);
-        void check();
-        void cook();
-        void finish();
+ public:
+    Automata();
+    STATES getState();
+    void getMenu();
+    void on();
+    void off();
+    void coin(int);
+    void cancel();
+    void choice(int);
+    void check();
+    void cook();
+    void finish();
 };
 
 #endif  // INCLUDE_AUTOMATA_H_
